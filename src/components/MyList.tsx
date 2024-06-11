a6de8713af48ada7d0567157b26dae254a577416
Abhinav Mahajan <mahajanabhinav.personal@gmail.com>
2024-06-11 12:41:52 +0530

    Fetched Data via API

--- src/components/MyList.tsx
+++ src/components/MyList.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import character from "../data/character.json";
 import CharacterListItem from "./CharacterListItem";
-import { FlatList } from "react-native";
+import { ActivityIndicator, FlatList } from "react-native";
 
 const MyList = () => {
+  const [loading, setLoading] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const makeAPICall = async () => {
+      setLoading(true);
+      const response = await fetch(`https://rickandmortyapi.com/api/character`);
+      const responseJson = await response.json();
+      setItems(responseJson.results);
+      setLoading(false);
+    };
+
+    makeAPICall();
+    return () => {};
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
   return (
     <FlatList
-      data={character.results}
+      data={items}
       renderItem={({ item, index }) => <CharacterListItem character={item} />}
       contentContainerStyle={{ gap: 50 }}
     />
