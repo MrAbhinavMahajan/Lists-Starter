f7f62106c6ce9a6f991ddc19d72a5a70ff34d438
Abhinav Mahajan <mahajanabhinav.personal@gmail.com>
2024-06-13 14:01:32 +0530

    Image Improvisations

--- src/components/CharacterListItem.tsx
+++ src/components/CharacterListItem.tsx
@@ -1,5 +1,12 @@
-import React, { memo } from "react";
-import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
+import React, { memo, useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
 import { Character } from "../types";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
@@ -12,10 +19,43 @@ export const CHARACTER_LIST_ITEM_HEIGHT = SCREEN_WIDTH;
 export const CHARACTER_LIST_ITEM_WIDTH = SCREEN_WIDTH;
 
 const CharacterListItem = ({ character }: CharacterListItem) => {
+  const [loading, setLoading] = useState(false);
+
+  const startLoading = () => setLoading(true);
+
+  const stopLoading = () => setLoading(false);
+
+  const onLoadStart = () => {
+    // Invoked on load start.
+    startLoading();
+  };
+
+  const onLoadEnd = () => {
+    // Invoked when load either succeeds or fails.
+    stopLoading();
+  };
+
+  const onError = ({ nativeEvent }) => {
+    // Invoked on load error.
+    console.log("onError: ", nativeEvent);
+    // ! Set Error Source
+    stopLoading();
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.name}>{character.name}</Text>
-      <Image source={{ uri: character.image }} style={styles.image} />
+      <Text style={styles.titleText}>{character.name}</Text>
+      <View style={styles.imageView}>
+        {loading && <ActivityIndicator size={"large"} style={styles.loader} />}
+        <Image
+          source={{ uri: character?.image, cache: "force-cache" }}
+          // loadingIndicatorSource={} // * displayed unless the source is ready to display
+          style={styles.image}
+          onError={onError}
+          onLoadStart={onLoadStart}
+          onLoadEnd={onLoadEnd}
+        />
+      </View>
     </View>
   );
 };
@@ -26,16 +66,27 @@ const styles = StyleSheet.create({
     height: CHARACTER_LIST_ITEM_HEIGHT,
     width: CHARACTER_LIST_ITEM_WIDTH,
   },
-  name: {
+  titleText: {
     fontSize: 20,
     fontWeight: "bold",
     color: "darkslategrey",
     alignSelf: "center",
     marginVertical: 10,
   },
+  imageView: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#dbeafe",
+    justifyContent: "center",
+  },
   image: {
     width: "100%",
     height: "100%",
+  },
+  loader: {
+    position: "absolute",
+    zIndex: 1000,
+    alignSelf: "center",
   },
 });
 
